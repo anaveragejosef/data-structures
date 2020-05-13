922377354678d2bdb442375087fa63ff7ae978ec
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-13 11:52:50 -0700

    Complete Functional Queue.js Program
    This commit contains the logic for .enqueue, .dequeue, and .size. This
    now passes all tests and does not use anything outside of the
    functional requirements.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,28 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var currentLastKey = someInstance.size();
+    storage[currentLastKey+1] = value;
   };
 
   someInstance.dequeue = function() {
+    var deletedValue = storage[1];
+    delete storage[1];
+    for (var key in storage) {
+      currentValue = storage[key];
+      delete storage[key];
+      storage[key-1] = currentValue;
+    }
+    return deletedValue;
   };
 
   someInstance.size = function() {
+    // create counter
+    var length = 0;
+    for (var key in storage) {
+      length++;
+    }
+    return length;
   };
 
   return someInstance;
