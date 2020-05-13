a965e01f7d8438555d71be34de3d032b73b91987
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-13 15:49:00 -0700

    Complete the Functional Stack.js Program
    - Wrote out the psuedocode so that I would have a clear picture of what
    needs to be done.
    - Once that was written, I translated it into JS
    - I used Size as a helper function in the other two methods

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,33 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // Will add a new value to the top of the storage obj
+    // Find the numeric key of the last item
+    var lastIndex = someInstance.size();
+    // Add the new value with a key plus one
+    storage[lastIndex + 1] = value;
   };
 
   someInstance.pop = function() {
+    // Will remove the value on the top of storage
+    // Find the numeric key of the last item
+    var lastIndex = someInstance.size();
+    // Remove the value from the object
+    var removalValue = storage[lastIndex];
+    delete storage[lastIndex];
+    return removalValue;
   };
 
   someInstance.size = function() {
+    // Return the number of items in storage
+    // Create a counter
+    var length = 0;
+    // Increase count for each item in the obj
+    for (var key in storage) {
+      length++;
+    }
+    // Return count
+    return length;
   };
 
   return someInstance;
