b94a3982fa6ff995cf9001788c1a378bfefd4f7d
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-13 16:19:06 -0700

    Add Logic for Protoypal Stack.js
    This commit adds the logic for the prototypal function to pass the test
    suite. The main difference in this version from the previous one is the
    use of Object.create vs extend.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,37 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // Use obj create to create a new instance
+  var newStack = Object.create(stackMethods);
+  // Add storage object
+  newStack.storage = {};
+  // Return the new instance
+  return newStack;
 };
 
 var stackMethods = {};
+
+// Create methods and add them into stackMethods
+
+stackMethods.push = function(value) {
+  var lastIndex = this.size();
+  this.storage[lastIndex + 1] = value;
+};
+
+stackMethods.pop = function() {
+  var lastIndex = this.size();
+  var removedItem = this.storage[lastIndex];
+  delete this.storage[lastIndex];
+  return removedItem;
+};
+
+stackMethods.size = function() {
+  var length = 0;
+  for (var key in this.storage) {
+    length++;
+  }
+  return length;
+};
 
 
