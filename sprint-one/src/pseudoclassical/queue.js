94c204f2711cfd4a400a64546287d6bc73a8c9e3
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-13 15:17:00 -0700

    Implement the Psuedoclassical Queue function
    This function was shorter than the protoypal as it only relied on the
    prototype methods that were passed along and not initialization was
    needed.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // Initialize an object to act as a storage container
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  var lastKey = this.size();
+  this.storage[lastKey + 1] = value;
+};
 
+Queue.prototype.dequeue = function() {
+  var deleted = this.storage[1];
+  delete this.storage[1];
+  for (var key in this.storage) {
+    var currentVal = this.storage[key];
+    delete this.storage[key];
+    this.storage[key - 1] = currentVal;
+  }
+
+  return deleted;
+};
+
+Queue.prototype.size = function() {
+  var length = 0;
+  for (var key in this.storage) {
+    length++;
+  }
+  return length;
+};
