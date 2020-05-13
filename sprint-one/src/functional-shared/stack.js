857c3cea7705ab4663d37449cd6efe98a5bc15c8
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-13 16:07:39 -0700

    Adds Logic for the Functional-Shared Stack.js Version
    This commit contains the following:
    - Psuedocode to determine out what will go where and when.
    - It no longer calls methods individually but assigns them to
    stackMethods.
    - It uses the keyword "this" as requested but this cases a test to
    fails. I believe that this test is not for this function.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,42 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // Create a new instance of Stack
+  var newStack = {};
+  // Add storage to stack
+  newStack.storage = {};
+  // Use extend to provide methods to this instance
+  _.extend(newStack, stackMethods);
+  return newStack;
 };
 
 var stackMethods = {};
 
+// Add stack methods push, pop, and size to stackMethods
+stackMethods.push = function (value) {
+  // Find the last index in object
+  var lastIndex = this.size();
+  // Add the value to the index plus 1
+  this.storage[lastIndex + 1] = value;
+};
 
+stackMethods.pop = function () {
+  // Find the last index in the object
+  var lastIndex = this.size();
+  // Delete this value
+  var deletedItem = this.storage[lastIndex];
+  delete this.storage[lastIndex];
+  // Return the deleted item
+  return deletedItem;
+};
+
+stackMethods.size = function () {
+  // Create a counter variable
+  var length = 0;
+  // Iterate through the storage object
+  for (var key in this.storage) {
+    // Increment the counter
+    length++;
+  }
+  return length;
+};
