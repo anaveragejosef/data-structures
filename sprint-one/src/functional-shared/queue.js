f2868a7e12e044f7e50379ae5c2dae9a80489956
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-13 12:23:42 -0700

    Add Beginnings of Queue in Funcitonal-Shared
    This commit will add the following:
    - Pseudocode structure
    - Build out the initial function using _.extend and a return call
    - Re-build both size and enqueue, using "this"

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue.storage = {};
+  _.extend(newQueue, queueMethods);
+  return newQueue;
 };
 
+// Create an object that holds the methods that will be shared by all instances of the class
 var queueMethods = {};
+// Use the keyword this in your methods
+// Will contain enqueue
+queueMethods.enqueue = function(value) {
+  // set the last current key to the length
+  var lastKey = this.size();
+  this.storage[lastKey + 1] = value;
+};
+// Will contain dequeue
+queueMethods.dequeue = function() {
+
+};
+// Will contain size
+queueMethods.size = function() {
+  // set length counter
+  var length = 0;
+  // iterate through storage container
+  for (var key in this.storage) {
+    // increase length
+    length++;
+  }
+  // return total size
+  return length;
+};
 
 
