bea04221778112d0faf71ae7857b2bb63c9a44cc
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-14 18:06:11 -0700

    Complete the Functional Implementation of LinkedList
    This commit contains the logic for the methods addToTail, removeHead,
    and contains. Once this was implemented all tests passed. Also we
    were able to determine which time complexities each would involve.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,45 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //create new Node with Node function and pass in input value
+    let newNode = Node(value);
+    //point tail to node
+    if (list.tail) {
+      //create variable to remember the previous Node
+      let previousNode = list.tail;
+      //the previous's node's next to new node
+      previousNode.next = newNode;
+    } else {
+    //point head to node
+      list.head = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    // Create temp variable that stores where .head is pointed
+    var deletedHead = list.head;
+    // Point head to current list.head's next node
+    list.head = deletedHead.next;
+    // Return temp variable value
+    return deletedHead.value;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, node) {
+    //set node = node, if node is not passed in, set it to list.head
+    node = node || list.head;
+    //if target matches node.value
+    if (target === node.value) {
+      //return true
+      return true;
+    }
+    //if node.next is equal to list.tail
+    if (node === list.tail) {
+      //return false;
+      return false;
+    }
+    //recursively run containst
+    return list.contains(target, node.next);
   };
 
   return list;
@@ -24,6 +57,16 @@ var Node = function(value) {
   return node;
 };
 
-/*
+/*n
  * Complexity: What is the time complexity of the above functions?
+ * list.removeHead - O(1)
+ * list.addToTail - O(1)
+ * list.contains - O(n)
  */
+// Specification - IOCE
+// Justification - What is the purpose of this function
+// Explanation - Relationship btwn input and output
+// Visualization - Whiteboard/Draw a plan for solving the problem
+// Approximation - Psuedocode
+// Verification - Walk through psuedocode with data
+// Implementation - Write code
