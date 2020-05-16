aa63d4c873e815cf35d0d953ec0a8a091f5dd2f4
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-15 20:42:42 -0700

    Add Protoypal Implementation of Set
    This commit uses objects in order to implement Set in the most effective
    manner. This allows all time complexities to be linear.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  //add item to storage
+  this._storage[item] = 'set';
 };
 
 setPrototype.contains = function(item) {
+  // Check if item exists in storage
+  if (this._storage[item]) {
+    // Return true
+    return true;
+  }
+  // Else, does not exist
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  //delete the obj at key item
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ add - Linear
+ contains - Linear
+ remove - Linear
  */
