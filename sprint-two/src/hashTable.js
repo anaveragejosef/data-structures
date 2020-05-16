aa38bb9b9b0ba90f03c3a433aa08f04f2def7170
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-15 21:51:50 -0700

    Add Beginning Work for hashTable.js
    This commit has the beginnings for hashTable. We are working to
    implement the helper functions in way that allows for collusions
    at this point.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,14 +5,23 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // Use limitedArray.set
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //returning the value
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
+  // Use limitedArray.each to iterate through array
+  // CB =
+  // Check if index matches current index
+  //
 };
 
 
@@ -22,5 +29,3 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
