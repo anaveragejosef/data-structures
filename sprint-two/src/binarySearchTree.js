45555b9b904afc7ba1efd2cea5081e9d7f62bba2
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-16 15:35:51 -0700

    Add the .insert Function to binarySearchTree
    This commit contains the logic needs for the .insert method to pass all
    tests in the spec.
    
    This class is being written in the prototypal instantiation pattern.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,44 @@
 var BinarySearchTree = function(value) {
+  // Object.create()
+  var newTree = Object.create(binaryMethods);
+  // Create our properties
+  // left, right, value
+  newTree.left = null;
+  newTree.right = null;
+  newTree.value = value;
+  // Return the object
+  return newTree;
+};
+
+// Create binaryMethods = {}
+var binaryMethods = {};
+
+// Create properties tied to binaryMethods
+binaryMethods.insert = function(value) {
+  var newNode = BinarySearchTree(value);
+  //if (value > this.value)
+  if (value > this.value) {
+    var direction = 'right';
+  } else {
+    var direction = 'left';
+  }
+  //if (direction) exists
+  if (this[direction]) {
+    //call the function again on direction.insert(value)
+    this[direction].insert(value);
+  } else {
+    //else put this value in direction
+    this[direction] = newNode;
+  }
+};
+
+binaryMethods.contains = function(value) {
+  // Returns boolean
+
+};
+
+binaryMethods.depthFirstLog = function(cb) {
+
 };
 
 
