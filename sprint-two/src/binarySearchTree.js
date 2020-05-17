6077c24a8a3f1c8a606fbe6e4278cfa0452c489a
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-16 22:36:54 -0700

    Completes the binarySearchTree Function
    This commit contains the last two methods for the binary search. this
    allows for all of the tests to pass in the spec.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -15,7 +15,6 @@ var binaryMethods = {};
 
 // Create properties tied to binaryMethods
 binaryMethods.insert = function(value) {
-  var newNode = BinarySearchTree(value);
   //if (value > this.value)
   if (value > this.value) {
     var direction = 'right';
@@ -27,21 +26,46 @@ binaryMethods.insert = function(value) {
     //call the function again on direction.insert(value)
     this[direction].insert(value);
   } else {
+    var newNode = BinarySearchTree(value);
     //else put this value in direction
     this[direction] = newNode;
   }
 };
 
 binaryMethods.contains = function(value) {
+  // Check if current tree contains value
+  if (this.value === value) {
+    return true;
+  }
+  if (this.left && this.left.contains(value)) {
+    return true;
+  }
+  if (this.right && this.right.contains(value)) {
+    return true;
+  }
   // Returns boolean
-
+  return false;
 };
 
 binaryMethods.depthFirstLog = function(cb) {
-
+  //put current node value as input in cb function
+  var result = cb(this.value);
+  //iterate through left
+  if (this.left) {
+    //recursive call
+    this.left.depthFirstLog(cb);
+  }
+  //iterate through right
+  if (this.right) {
+    //recursive call
+    this.right.depthFirstLog(cb);
+  }
+  //return result;
+  return result;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * All linear - O(n);
  */
